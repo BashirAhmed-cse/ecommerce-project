6122941270ba1d5c897f781066567d24556f7468
bashir <bashirahmed.cse@gmail.com>
2024-02-26 00:24:13 +0600

    create new user function

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import cookieParser from 'cookie-parser';
 //utiles
 
 import connectDb from "./utils/db.js";
-
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -22,8 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.send("Hellow world");
-});
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
