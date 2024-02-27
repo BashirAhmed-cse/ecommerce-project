a8d3e0c2e3b9635d6b5337e3810014c4df489991
bashir <bashirahmed.cse@gmail.com>
2024-02-27 16:28:12 +0600

    user update and user delete by admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,25 @@
 import express from "express";
-import {createUser, loginUser, logoutUser} from '../controllers/userController.js';
+import {createUser, loginUser, logoutUser, 
+    getAllUsers, getCurrentUserProfile, 
+    updateCurrentUserProfile, deleteUserById
+} from '../controllers/userController.js';
+
+import { authenticate, authorizeAdmin } from "../middlewars/authMiddleware.js";
+
 const router =  express.Router();
 
-router.route("/").post(createUser)
+router.route("/")
+.post(createUser)
+.get(authenticate, authorizeAdmin, getAllUsers);
+
 router.post('/auth', loginUser);
 router.post('/logout', logoutUser);
 
+router.route('/profile')
+.get(authenticate, getCurrentUserProfile)
+.put(authenticate, updateCurrentUserProfile);
+
+//admin routes
+router.route('/:id').delete(authenticate, authorizeAdmin, deleteUserById)
 
 export default router;
